2524366a74caa226784cfcaa15f777ee3012cb13
Mai Tran <maimaimaihoabinh@gmail.com>
2024-06-07 09:06:57 +0700

    Mai_profile 07.06.24

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import Login from "./layout/Login";
 import Navbar from "./layout/Navbar";
 import Home from "./layout/Home";
 import Register from "./layout/Register";
+import ProfileCandidate from "./layout/ProfileCandidate";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
       <Router>
+        <ToastContainer />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
+          <Route path="/profile" component={ProfileCandidate} />
         </Switch>
       </Router>
     </div>
